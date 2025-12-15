f983adb200b2e12fa14ed104a3be279a4bf6d108
Melker Widén <melker.widen@gmail.com>
2025-12-16 00:13:20 +0100

    A lil progress

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 The supreme voting system of truth and consensus
 
 Spec: https://cpu.dsek.se/doc/rostsystem-SmJ70AMu7P
+there is a plan
+
+https://cpu.dsek.se/doc/rostsystem-Y7XSstUiED/
 
---
+++ cmd/proxy/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"github.com/Dsek-LTH/decidr/internal/routes"
+	"github.com/Dsek-LTH/decidr/internal/templates"
+)
+
+func logginMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf(
+			"%s -> %s",
+			r.RemoteAddr,
+			r.URL.String(),
+		)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	templateRenderer := templates.NewTemplateRenderer()
+
+	mux := http.NewServeMux()
+
+	routes.RegisterRoutes(mux, templateRenderer)
+
+	fs := http.FileServer(http.Dir("./web/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	logged := logginMiddleWare(mux)
+
+	log.Println("Server listening on :11337")
+	http.ListenAndServe(":11337", logged)
+}
